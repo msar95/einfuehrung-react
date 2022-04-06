9c0492716640f45ef9a59ef7f49eb1905c31c103
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-06 19:57:40 +0200

    added constructor

--- src/app/core-components/Headline.jsx
+++ src/app/core-components/Headline.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import './Headline.css';
 
 export class Headline extends React.Component{
+    constructor(props){
+        super(props);
+    }
     render() {
         return (
             <h1 className="headline">{this.props.headline}</h1>
